682b92bdce7b3fea67fa3a66bbd7c18eb5f9f3ea
Matthias Riegler <me@xvzf.tech>
2019-05-16 10:40:47 +0200

    Stringer interface for the storage driver not needed anymore

--- pkg/storage/types.go
+++ pkg/storage/types.go
@@ -2,11 +2,6 @@ package storage
 
 import "github.com/elastifeed/es-pusher/pkg/document"
 
-// Stringer interface for documents
-type Stringer interface {
-	String() string
-}
-
 // Storage interface
 type Storage interface {
 	AddDocuments(index string, docs []document.Document) error
