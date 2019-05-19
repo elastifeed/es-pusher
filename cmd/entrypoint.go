19072d51fe0473599e048c69da963d3c5229ebf9
Matthias Riegler <me@xvzf.tech>
2019-05-19 14:40:19 +0200

    Added configuration via environment variables

--- cmd/entrypoint.go
+++ cmd/entrypoint.go
@@ -1,21 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/elastic/go-elasticsearch/v7"
 	"github.com/elastifeed/es-pusher/pkg/api"
 	"github.com/elastifeed/es-pusher/pkg/storage"
 )
 
+// main entry point for es-pusher
+//
+// Possible Configuration options (via environment):
+//    - ES_ADDRESSES (e.g. ["http://host0:9200", "http://host1:9200", ...])
+//    - API_BIND (e.g. ":9000")
 func main() {
+
+	var addrs []string
+
+	if json.Unmarshal([]byte(os.Getenv("ES_ADDRESSES")), &addrs) != nil {
+		log.Fatal("Configuration error, check ES_ADDRESSES")
+	}
+
+	log.Println(addrs)
 
 	// Connect to a specified Elasticsearch instance
 	s := storage.NewES(elasticsearch.Config{
-		Addresses: []string{
-			"http://127.0.0.1:9200",
-		},
+		Addresses: addrs,
 	})
 
 	// Create new Rest Api Endpoint based on the previously connected elasticsearch storage engine
@@ -24,5 +37,5 @@ func main() {
 	http.HandleFunc("/add", rAPI.AddDocuments)
 
 	// Run forever and exit on error
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(os.Getenv("API_BIND"), nil))
 }
